03d50c42fab1a1c48ffc36e00a5e87f97b326124
Eric Wong <ewong@ip-10-160-59-162.us-west-1.compute.internal>
2013-07-17 04:31:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Lanbo
+=====
+
+Simple library to interface with L293D motor controller with the raspberry pi.
+
+TODO
+====
+
+* PWM does not work for some reason.
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lanbo',
+    version='0.1',
+    description='Library to interface with L293D motor controller',
+    author='Eric Wong',
+    url='https://github.com/etdub/lanbo',
+    packages=['lanbo'],
+    )
